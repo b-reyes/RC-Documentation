c3e5745ab385814ce6ddeec34c408dedac2eab04
b-reyes <reyesb123@gmail.com>
2023-03-02 15:44:59 -0700

    change theme to sphinx_book_theme

--- docs/conf.py
+++ docs/conf.py
@@ -2,16 +2,15 @@ from recommonmark.parser import CommonMarkParser
 
 project = "Research Computing\nUniversity of Colorado Boulder"
 
+html_title = "Research Computing University of Colorado Boulder"
+
 master_doc = 'index'
 
-source_parsers = {
-    '.md': CommonMarkParser,
-}
-
 extensions = [
-    'sphinx_markdown_tables',
+    'sphinx_markdown_tables', 'myst_parser', 'sphinx_copybutton'
 ]
 
 source_suffix = ['.rst', '.md']
 
-html_theme = 'sphinx_rtd_theme'
+# html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_book_theme'
